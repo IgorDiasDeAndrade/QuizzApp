a10616c5a595bbaa74685129a0e8b54f1a1cdc21
igordiasdeandrade <igor.d.andrade@live.com>
2022-06-03 01:08:01 +0000

    Adicionado controller para backoffice de profiles e admins, adicionadas rotas e herança de layout nos controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   namespace :profiles_backoffice do
     get 'welcome/index'
   end
-  namespace :profile_backoffice do
-    get 'welcome/index'
-  end
   namespace :admins_backoffice do
+    get 'welcome/index'
   end
   devise_for :profiles
   devise_for :admins
