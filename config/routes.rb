d2333661e1a8c51b90ef560ebc6ad760ccdf6f42
igordiasdeandrade <igor.d.andrade@live.com>
2022-06-03 00:20:25 +0000

    Gerado controlador para backoffice de admins e profiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :profiles_backoffice do
+    get 'welcome/index'
+  end
+  namespace :profile_backoffice do
+    get 'welcome/index'
+  end
+  namespace :admins_backoffice do
+  end
   devise_for :profiles
   devise_for :admins
   get 'welcome/index'
